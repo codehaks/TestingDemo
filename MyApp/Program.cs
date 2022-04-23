34ae299d77680f2b8fa5be13d0b3bbf5fe1a4ef9
Hakim Ghods <ghods.hakim@hotmail.com>
2022-04-23 22:03:19 +0430

    Add project files.

---
+++ MyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var text = File.ReadAllText(@"f:\numbers.txt"); 
+var textNumbers=text.Split(',');
+
+var numbers=new List<int>();
+foreach (var number in textNumbers)
+{
+    numbers.Add(int.Parse(number));
+}
+
+var orderedNumbers=numbers.OrderBy(x => x).ToList();
+
+var min=orderedNumbers.First();
+var max=orderedNumbers.Last();
+
+var mid = (min + max) / 2;
+
+Console.WriteLine(mid);
+
+Console.WriteLine("Done!");
+
+
+
